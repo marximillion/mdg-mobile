fdb263e5b3593ed5aca1393a99c66a46be7fb481
marximillion <marximillion@gmail.com>
2026-01-06 23:28:19 -0700

    [m1-max] - UPGRADE - transfer assets

---
+++ app/assets/images.ts
@@ -0,0 +1,26 @@
+export const images = Object.freeze({
+  // Background Images
+  background_image: require('./images/backgroundImage.jpeg'),
+  barangay_feud_logo: require('./images/2025-barangay-christmas-feud-transparent.png'),
+
+  // Car Images
+  csx: require('./images/cars/csx.jpg'),
+  csx_type_s: require('./images/cars/csx-type-s.png'),
+  crv: require('./images/cars/crv.jpg'),
+  odyssey: require('./images/cars/odyssey.png'),
+
+  // Random Images
+  clown: require('./images/clown.png'),
+
+  // Placeholder Images
+  no_image: require('./images/noImage.png'),
+
+  // JavaScript & jQuery images
+  check_cross: require('./images/check-cross.png'),
+  travelworthy_backdrop: require('./images/travelworthy-backdrop.jpg'),
+  travelworthy_logo: require('./images/travelworthy-logo.gif'),
+
+  // Peeps
+  ec_yellow_comic: require('./images/ec-yellow-comic.jpg'),
+  mdg_bald: require('./images/mdg-bald.jpg')
+});
