e190cd57e9e162584151f6dbe1891a8b1934d199
marximillion <marximillion@gmail.com>
2026-01-26 10:49:01 -0700

    [m1-max] - Organize image aassets

--- app/assets/images.ts
+++ app/assets/images.ts
@@ -1,7 +1,11 @@
+/**
+ * Copyright © MJDG 2026
+ */
+
 export const images = Object.freeze({
   // Background Images
-  background_image: require('./images/backgroundImage.jpeg'),
-  barangay_feud_logo: require('./images/2025-barangay-christmas-feud-transparent.png'),
+  background_image: require('./images/bg/backgroundImage.jpeg'),
+  barangay_feud_logo: require('./images/game/2025-barangay-christmas-feud-transparent.png'),
 
   // Car Images
   csx: require('./images/cars/csx.jpg'),
@@ -10,22 +14,24 @@ export const images = Object.freeze({
   odyssey: require('./images/cars/odyssey.png'),
 
   // Random Images
-  clown: require('./images/clown.png'),
+  clown: require('./images/misc/clown.png'),
 
   // Placeholder Images
-  no_image: require('./images/noImage.png'),
+  no_image: require('./images/misc/noImage.png'),
 
   // JavaScript & jQuery images
-  check_cross: require('./images/check-cross.png'),
-  travelworthy_backdrop: require('./images/travelworthy-backdrop.jpg'),
-  travelworthy_logo: require('./images/travelworthy-logo.gif'),
+  check_cross: require('./images/misc/check-cross.png'),
+  travelworthy_backdrop: require('./images/misc/travelworthy-backdrop.jpg'),
+  travelworthy_logo: require('./images/misc/travelworthy-logo.gif'),
 
   // Peeps
-  ec_yellow_comic: require('./images/ec-yellow-comic.jpg'),
-  mdg_bald: require('./images/mdg-bald.jpg'),
+  ec_yellow_comic: require('./images/peeps/ec-yellow-comic.jpg'),
+  mdg_bald: require('./images/peeps/mdg-bald.jpg'),
 
   // Logos
   logo_v1: require('./images/logos/lab3-logo-v1.png'),
   logo_v2: require('./images/logos/lab3-logo-v2.png'),
   logo_v3: require('./images/logos/lab3-logo-v3.png'),
+
+  emblem_v1: require('./images/logos/lab3-emblem-v1.png'),
 });
